6f6c1e2bcf66a13c83740eba346a9d59585bd858
Catalin Stanciu <catacsdev@gmail.com>
2018-02-15 20:53:13 +0100

    Add tests and api implementations

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,7 +35,12 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 RSpec.configure do |config|
+  # show all tests status
+  config.formatter = :documentation
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -65,6 +70,7 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include FactoryBot::Syntax::Methods
+  config.include RequestSpecHelper, type: :request
 
   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
   config.before(:suite) do
