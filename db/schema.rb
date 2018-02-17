b18b7c2128456ce3d920d7febedae512db82c48f
Catalin Stanciu <catacsdev@gmail.com>
2018-02-17 12:40:51 +0100

    Add uniqueness of email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180215195559) do
+ActiveRecord::Schema.define(version: 20180217073049) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20180215195559) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
